6f6693c8406d176b9c403d5672ebe310825bfeac
Adam Rowe <adaminsley@gmail.com>
2020-04-21 16:48:16 -0400

    design updates, hide keyboard on tap outside search

--- MovieSearch/Views/MainViewController.swift
+++ MovieSearch/Views/MainViewController.swift
@@ -23,28 +23,65 @@ class MainViewController: UIViewController {
         }
     }
 
-    lazy var spinnerView:UIActivityIndicatorView = {
+    lazy var footerView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+
+        let footerImageView = UIImageView(image: UIImage(named: "tmdb"))
+        footerImageView.contentMode = .scaleAspectFit
+        footerImageView.translatesAutoresizingMaskIntoConstraints = false
+
+        footerImageView.heightAnchor.constraint(equalToConstant: 36.0).isActive = true
+        footerImageView.widthAnchor.constraint(equalToConstant: 36.0).isActive = true
+
+        stackView.addArrangedSubview(footerImageView)
+
+        footerImageView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 8).isActive = true
+
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = "This product uses the TMDb API but is not endorsed or certified by TMDb."
+        label.adjustsFontSizeToFitWidth = true
+        label.font = UIFont.systemFont(ofSize: 10, weight: UIFont.Weight.thin)
+        label.textAlignment = .justified
+
+        stackView.addArrangedSubview(label)
+
+        stackView.widthAnchor.constraint(equalToConstant: moviesTableView.bounds.width - 16).isActive = true
+
+        label.trailingAnchor.constraint(equalTo: stackView.trailingAnchor).isActive = true
+
+        return stackView
+    }()
+
+    lazy var spinnerView: UIActivityIndicatorView = {
         let spinner = UIActivityIndicatorView(style: .large)
         spinner.translatesAutoresizingMaskIntoConstraints = false
         spinner.backgroundColor = .systemBackground
 
         view.addSubview(spinner)
 
-        spinner.centerXAnchor.constraint(equalTo: tmdbImage.centerXAnchor).isActive = true
-        spinner.centerYAnchor.constraint(equalTo: tmdbImage.centerYAnchor).isActive = true
+        spinner.centerXAnchor.constraint(equalTo: movieSearchImage.centerXAnchor).isActive = true
+        spinner.centerYAnchor.constraint(equalTo: movieSearchImage.centerYAnchor).isActive = true
 
         return spinner
     }()
 
     @IBOutlet var moviesTableView: UITableView!
     @IBOutlet var searchBar: UISearchBar!
-    @IBOutlet var tmdbImage: UIImageView!
+    @IBOutlet var movieSearchImage: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self,
+                                                                 action: #selector(dismissKeyboard) )
+        view.addGestureRecognizer(tap)
+
         searchBar.delegate = self
         moviesTableView.dataSource = self
+        moviesTableView.delegate = self
     }
 
     private func convertResponseToMovies(_ data: Data) -> [Movie] {
@@ -55,8 +92,16 @@ class MainViewController: UIViewController {
 
     private func showSearchResults(_ isHidden: Bool) {
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-            self.tmdbImage.isHiddenWithAnimation(isHidden)
+            self.movieSearchImage.isHiddenWithAnimation(isHidden)
             self.moviesTableView.isHiddenWithAnimation(!isHidden)
+        }
+    }
+
+    @objc func dismissKeyboard(_ sender: AnyObject) {
+        if let tapAction = sender as? UITapGestureRecognizer {
+            if tapAction.view != searchBar {
+                view.endEditing(true)
+            }
         }
     }
 }
@@ -77,6 +122,16 @@ extension MainViewController: UITableViewDataSource {
     }
 }
 
+extension MainViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 60.0
+    }
+
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return footerView
+    }
+}
+
 extension MainViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder()
@@ -86,7 +141,7 @@ extension MainViewController: UISearchBarDelegate {
         }
 
         spinnerView.startAnimating()
-        tmdbImage.isHidden = true
+        movieSearchImage.isHidden = true
 
         queryService.request(.search(matching: searchText)) { result in
             switch result {
